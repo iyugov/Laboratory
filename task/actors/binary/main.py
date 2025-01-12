9a7bd6f9515be3f934a073560b10398375b68377
iyugov <iyugov@gmail.com>
2025-01-12 19:55:07 +0300

    Add task on binary actor: type C, condition R != N

--- task/actors/binary/main.py
+++ task/actors/binary/main.py
@@ -2,12 +2,6 @@
 
 from type_c import TaskActorsBinaryTypeC
 
-solution_set = set()
-while True:
-    task = TaskActorsBinaryTypeC(True)
-
-    solution = task.solve()
-    if solution not in solution_set:
-        print(task)
-        print(solution)
-        solution_set.add(solution)
+task = TaskActorsBinaryTypeC(True)
+print(task)
+print(task.solve())
